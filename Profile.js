7bf85673b92c9dc8250b297e29e98909b597a642
sanjeewa <nilanthasanjeewa101@gmail.com>
2025-03-30 19:41:23 +0530

    Profile page created.

---
+++ Profile.js
@@ -0,0 +1,34 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const editNicknameButton = document.getElementById('editNickname');
+    const nicknameSpan = document.getElementById('nickname');
+    const nicknameInput = document.getElementById('nicknameInput');
+    const saveNicknameButton = document.getElementById('saveNickname');
+
+    editNicknameButton.addEventListener('click', function() {
+        nicknameSpan.style.display = 'none';
+        editNicknameButton.style.display = 'none';
+        nicknameInput.style.display = 'inline-block';
+        saveNicknameButton.style.display = 'inline-block';
+        nicknameInput.value = nicknameSpan.textContent;
+    });
+
+    saveNicknameButton.addEventListener('click', function() {
+        nicknameSpan.textContent = nicknameInput.value;
+        nicknameSpan.style.display = 'inline-block';
+        editNicknameButton.style.display = 'inline-block';
+        nicknameInput.style.display = 'none';
+        saveNicknameButton.style.display = 'none';
+    });
+
+    const backButton = document.querySelector('.back-button');
+    const playAgainButton = document.querySelector('.play-again-button');
+
+    backButton.addEventListener('click', function() {
+        window.location.href = 'GameInterface.html';
+    });
+
+    playAgainButton.addEventListener('click', function() {
+        window.location.href = 'game.html';
+    });
+    const backgroundMusic = document.getElementById('backgroundMusic');
+});
